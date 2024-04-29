84be87788f9a754299b56e72560b4f9bcfca6922
scott <scottysora@gmail.com>
2024-04-29 16:26:39 -0400

    complete simple add function

---
+++ exercises/add.js
@@ -0,0 +1,22 @@
+function add(number1, number2) {
+  var result = number1 + number2;
+
+  console.log(`The sum of x and y is ${result}`);
+}
+
+var x = 5;
+var y = 6;
+
+add(x, y);
+
+
+
+function bake(ingredient1, ingredient2, ingredient3) {
+    if(ingredient1 =="apples"){
+        console.log("apple pie");
+    }
+}
+
+bake()
+
+//add a class to the form style
