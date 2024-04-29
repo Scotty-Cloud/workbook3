322069076fd9099fa6b260a1edfe52e924e91c57
scott <scottysora@gmail.com>
2024-04-29 15:07:57 -0400

    complete exercise 1 of wb3

---
+++ exercises/simple-function.js
@@ -0,0 +1,13 @@
+function favoriteThings() {
+  const name = "scotty";
+  const favMovie = "Cowboy Bebop: Knocking on Heavens Door";
+  const favBand = "Celldweller";
+
+  console.log(`My name is ${name}`);
+
+  console.log(`My favorite movie is ${favMovie}`);
+
+  console.log(`My favorite band is ${favBand}`);
+}
+
+favoriteThings();
